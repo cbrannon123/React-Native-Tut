cab57cd9c603c57dafc46ec0d9d800c4d859a1c7
Casey Brannon <cbrannon412@gmail.com>
2019-12-05 17:43:22 -0500

    color changer with useReducer

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,49 +1,78 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 import ColorCounter from '../components/ClororCounter';
 
 const COLOR_INCREMENT = 15;
+// way with useState()
+// const SquareScreen = () => {
+//     const [red, setRed] = useState(0);
+//     const [green, setGreen] = useState(0);
+//     const [blue, setBlue] = useState(0);
+
+//     const setColor = (color, change) => {
+//         //color is === red green blue
+//         //change ==== + 15 or -15(colorincrement)
+//         switch (color) {
+//             case 'red':
+//             red + change > 255 || red + change < 0 ? null : setRed(red + change);
+//             return;
+//             case 'green':
+//             green + change > 255 || green + change < 0 ? null : setGreen(green + change);
+//             return;
+//             case 'blue':
+//             blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change);
+//             return;
+//         }
+
+//     };
+
+//state === to obj color, number
+//action is how we are changing state obj(color and number)
+const reducer = (state, action) => {
+    switch (action.colorToChange) {
+        case 'red':
+            //never modife 1st arg directly//creates new obj
+            return { ...state, red: state.red + action.amount };
+        case 'green':
+            return { ...state, green: state.green + action.amount };
+        case 'blue':
+            return { ...state, blue: state.blue + action.amount };
+        default:
+            //if cases are not matched, return obj
+            return state;
+    }
+};
 
 const SquareScreen = () => {
-    const [red, setRed] = useState(0);
-    const [green, setGreen] = useState(0);
-    const [blue, setBlue] = useState(0);
+    //dispatch === RunMyReducer
+    const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 });
+    const { red, green, blue } = state;
 
-    const setColor = (color, change) => {
-        //color is === red green blue
-        //change ==== + 15 or -15(colorincrement)
-        switch (color) {
-            case 'red':
-            red + change > 255 || red + change < 0 ? null : setRed(red + change);
-            return;
-            case 'green':
-            green + change > 255 || green + change < 0 ? null : setGreen(green + change);
-            return;
-            case 'blue':
-            blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change);
-            return;
-        }
 
-    };
 
     return (
         <View>
             <ColorCounter
-                onIncrease={() => setColor('red', COLOR_INCREMENT)}
-                onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)}
+                onIncrease={() => dispatch({ colorToChange: 'red', amount: COLOR_INCREMENT })}
+                onDecrease={() => dispatch({ colorToChange: 'red', amount: -1 * COLOR_INCREMENT })}
                 color='Red'
             />
             <ColorCounter
-                onIncrease={() => setColor('blue', COLOR_INCREMENT)}
-                onDecrease={() => setColor('blue', -1 * COLOR_INCREMENT)}
+                onIncrease={() => dispatch({ colorToChange: 'blue', amount: COLOR_INCREMENT })}
+                onDecrease={() => dispatch({ colorToChange: 'blue', amount: -1 * COLOR_INCREMENT })}
                 color="Blue"
             />
             <ColorCounter
-                onIncrease={() => setColor('green', COLOR_INCREMENT)}
-                onDecrease={() => setColor('green', -1 * COLOR_INCREMENT)}
+                onIncrease={() => dispatch({ colorToChange: 'green', amount: COLOR_INCREMENT })}
+                onDecrease={() => dispatch({ colorToChange: 'green', amount: -1 * COLOR_INCREMENT })}
                 color="Green"
             />
-            <View style={{ height: 150, width: 150, backgroundColor: `rgb(${red},${green},${blue})` }} />
+            <View style={{
+                height: 150,
+                width: 150,
+                backgroundColor: `rgb(${red},${green},${blue})`
+            }}
+            />
         </View>
     );
 };
