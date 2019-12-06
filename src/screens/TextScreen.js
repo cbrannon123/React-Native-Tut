6497ec28925a1c5391154e1b040ca962987296a2
Casey Brannon <cbrannon412@gmail.com>
2019-12-06 16:19:42 -0500

    textScreen w/ condition

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { State } from 'react-native-gesture-handler';
 
 const TextScreen = () => {
+    const [name, setName] = useState('');
+
     return (
         <View>
-            <Text>Text Screen</Text>
+            <Text>Enter Password:</Text>
+            <TextInput
+                style={styles.input}
+                autoCapitalize="none"
+                autoCorrect={false}
+                value={name}
+                onChangeText={(newValue) => setName(newValue)}
+            />
+           {name.length > 4 ? null : <Text>Pw must be 5 char long.</Text>}
+
         </View>
     )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    input: {
+        margin: 15,
+        borderColor: 'black',
+        borderWidth: 1
+    }
+});
 
 export default TextScreen;
