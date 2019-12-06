7f30a5146aa5842a21e041f6de10e68a4246c5b1
Casey Brannon <cbrannon412@gmail.com>
2019-12-06 17:10:07 -0500

    ReactNative Flex demo

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -1,14 +1,42 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
     return (
-        <View>
-            <Text>Box Screen</Text>
+        <View style={styles.viewStyle}>
+            <View style={styles.viewOne}/>
+            <View  style={styles.viewTwo}/>
+            <View  style={styles.viewThree}/>
+
         </View>
     );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    viewStyle: {
+        borderWidth: 3,
+        borderColor: 'black',
+        height: 200,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    viewOne: {
+        width: 50,
+        height: 50,
+        backgroundColor:'red'
+
+    },
+    viewTwo: {
+        width: 50,
+        height: 50,
+        backgroundColor:'blue',
+        top: 50
+    },
+    viewThree: {
+        width: 50,
+        height: 50,
+        backgroundColor:'green'
+    },
+});
 
 export default BoxScreen;
