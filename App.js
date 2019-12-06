6e6f41fdc5f63691dd31e989c7b12d6108d4215b
Casey Brannon <cbrannon412@gmail.com>
2019-12-06 16:25:45 -0500

    boxdemo

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import CounterScreen from './src/screens/CounterScreen';
 import AddColorScreen from './src/screens/AddCollorScreen';
 import SquareScreen from './src/screens/SquareScreen';
 import TextScreen from './src/screens/TextScreen';
+import BoxScreen from './src/screens/BoxScreen';
 
 const navigator = createStackNavigator(
   {
@@ -17,7 +18,8 @@ const navigator = createStackNavigator(
     Counter: CounterScreen,
     Color: AddColorScreen,
     Square: SquareScreen,
-    Text: TextScreen
+    Text: TextScreen,
+    Box: BoxScreen
   },
   {
     initialRouteName: 'Home',
